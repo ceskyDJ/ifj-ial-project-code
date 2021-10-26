8064617ea13cf3da89fb6dad025845bc40b459bf
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2021-10-27 01:57:05 +0200

    Add Unity framework for unit testing #18

---
+++ unity_config.h
@@ -0,0 +1,20 @@
+/**
+ * @file unity_config.h
+ * Unity framework configurations
+ *
+ * Unity framework uses configuration throw defining specific macros. The full list of macros is in docs at Unity's
+ * Github repository at https://github.com/ThrowTheSwitch/Unity/blob/master/docs/UnityConfigurationGuide.md.
+ *
+ * IFJ and IAL project (IFJ21 compiler)
+ * Team: 128 (variant II)
+ *
+ * @author Michal Šmahel (xsmahe01)
+ */
+
+#ifndef _UNITY_CONFIG_H_
+#define _UNITY_CONFIG_H_
+
+// More beautiful output with some colored parts
+#define UNITY_OUTPUT_COLOR
+
+#endif //_UNITY_CONFIG_H_
