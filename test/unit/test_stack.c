cff3671c9c4a6b755804059d28fe48e0840afc87
Martin Havlík <omnitex.git@gmail.com>
2021-11-12 22:36:15 +0100

    test_stack: some fixes FIXUP

--- test/unit/test_stack.c
+++ test/unit/test_stack.c
@@ -3,49 +3,49 @@
 
 void test_stack_init(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     TEST_ASSERT_NOT_NULL(stack);
 }
 
 void test_stack_dispose(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     stack_dispose(s);
     TEST_PASS();
 }
 
 void test_stack_push(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     symtable_t *t = symtable_create();
-    stack_push(stack, t);
+    stack_push(&stack, t);
     TEST_ASSERT(stack.top != NULL);
     TEST_ASSERT(stack.size == 1);
 }
 
 void test_stack_pop(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     TEST_ASSERT_NOT_NULL(stack);
 }
 
 void test_first_active(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     symtable_t *t = symtable_create();
-    stack_push(stack, t);
-    first_active(stack);
+    stack_push(&stack, t);
+    first_active(&stack);
     TEST_ASSERT(stack.act == stack.top);
 }
 
 void test_next_active(void){
     stack_t stack;
-    stack_init(stack);
+    stack_init(&stack);
     symtable_t *t = symtable_create();
     symtable_t *t2 = symtable_create();
-    stack_push(stack, t);
-    stack_push(stack, t2);
-    first_active(stack);
-    next_active(stack);
+    stack_push(&stack, t);
+    stack_push(&stack, t2);
+    first_active(&stack);
+    next_active(&stack);
     TEST_ASSERT(stack.act == *t2);
 }
