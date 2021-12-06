8c9f5ada15219386eb562e2d54a58a64a719f3cf
Michal Šmahel <48548230+ceskyDJ@users.noreply.github.com>
2021-12-06 20:23:00 +0100

    Add built-in functions into global symtable at start #70

--- src/main.c
+++ src/main.c
@@ -12,9 +12,47 @@
 #include "exit_codes.h"
 #include "logger.h"
 #include "parser.h"
+#include "symtable.h"
+#include "identifier.h"
+#include "string.h"
 
-int main() {
+static void add_builtin_function(symtable_t *symtable, char *name, char *param, char *retval)
+{
+    identifier_t *fun;
+    char *param_alloc;
+    char *retval_alloc;
 
+    param_alloc = malloc(strlen(param) + 1);
+    retval_alloc = malloc(strlen(retval) + 1);
+    if (!param_alloc || !retval_alloc)
+        exit(EINTERNAL);
+
+    strcpy(param_alloc, param);
+    strcpy(retval_alloc, retval);
+
+    fun = symtable_add(symtable, name);
+    fun->type = FUNCTION;
+    fun->line = 0;
+    fun->character = 0;
+    fun->fun.defined = true;
+    fun->fun.param = param_alloc;
+    fun->fun.retval = retval_alloc;
+}
+
+static void init_builtin_functions(symtable_t *symtable)
+{
+    add_builtin_function(symtable, "reads", "", "s");
+    add_builtin_function(symtable, "readi", "", "i");
+    add_builtin_function(symtable, "readn", "", "n");
+    add_builtin_function(symtable, "write", "write", "");
+    add_builtin_function(symtable, "tointeger", "n", "i");
+    add_builtin_function(symtable, "substr", "snn", "s");
+    add_builtin_function(symtable, "ord", "si", "i");
+    add_builtin_function(symtable, "chr", "i", "s");
+}
+
+int main()
+{
     context_t ctx;
     int ret;
 
@@ -46,7 +84,7 @@ int main() {
     if (!symqueue)
         exit(EINTERNAL);
 
-    // TODO fill global_symtable with builtin functions
+    init_builtin_functions(global_symtable);
 
     ret = symstack_push(symstack, global_symtable);
     if (ret < 0)
